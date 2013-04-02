a3af9ddd5b0da91259bbd19e6e45b25b39c5ebf6
Grigory <grtkachenko@gmail.com>
2013-04-03 01:38:35 +0400

    Working version with forbidden functions

--- dupme/main.c
+++ dupme/main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, char* argv[]) {
-    printf("%d args\n", argc);
+    int k;
+    char *buffer;
+    int cur_len;
+    if (argc < 2) {
+        return 1;
+    }
+    k = atoi(argv[1]);
+    if (k < 1) {
+        return 2;
+    }
+    buffer = (char *) malloc(k);
+    cur_len = 0;
+    
     int i;
-    for (i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
+    for (i = 2; i < argc; i++) {
+        if (strlen(argv[i]) <= k) {
+             printf("%s\n%s\n", argv[i], argv[i]);
+        }
     }
     return 0;
 }
