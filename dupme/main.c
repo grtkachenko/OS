66512bf2c48a71fc0a72e2862525eb7225f17fbf
Grigory <grtkachenko@gmail.com>
2013-04-04 12:47:55 +0400

    Delete unnecessary comments and reformate some code

--- dupme/main.c
+++ dupme/main.c
@@ -23,19 +23,15 @@ int main(int argc, char* argv[]) {
         PRINT, IGNORE
     } current_state;
     char* buffer;
-    int k = string_to_int(argv[1]);
+    int k = string_to_int(argv[1]), len = 0;
     buffer = malloc(k + 1);
-    int len = 0;
     current_state = PRINT;
 
     while (1) {
         int read_res = read(IN, buffer + len, k + 1 - len);
-//        print(k + 1);
-//        printf(" %d", len);
         if (read_res == 0) {
             if (current_state == PRINT && len > 0 && len <= k) {
-                buffer[len] = '\n';
-                len++;
+                buffer[len++] = '\n';
                 print_string_prefix(buffer, len);
                 print_string_prefix(buffer, len);
             }
@@ -45,12 +41,11 @@ int main(int argc, char* argv[]) {
         int right = read_res + len;
         while (left < right) {
             if (buffer[left] == '\n') {
+                left++;
                 if (current_state == PRINT) {
-                    left++;
                     print_string_prefix(buffer, left);
                     print_string_prefix(buffer, left);
                 } else {
-                    left++;
                     current_state = PRINT;
                 }
                 memmove(buffer, buffer + left, right - left);
