dfaca536cbea88b06fc42c88d834b8486aaad3a5
Grigory <grtkachenko@gmail.com>
2013-06-17 01:23:36 +0400

    Minimized memmove count

--- dupme/main.c
+++ dupme/main.c
@@ -7,19 +7,19 @@ typedef enum {
     PRINT, IGNORE
 } state;
 
-void print_string_prefix(char* buffer, int count) {
-    int num_ok = 0;
-    while (num_ok < count) {
-        int cur = write(OUT, buffer + num_ok, count - num_ok);
+void print_string_prefix(char* buffer, int left, int right) {
+    int cur_left = left;
+    while (cur_left < right) {
+        int cur = write(OUT, buffer + cur_left, right - cur_left);
         if (cur > 0) {
-            num_ok += cur;
+            cur_left += cur;
         }
     }
 }
 
-void print_string_twice(char* buffer, int count) {
-    print_string_prefix(buffer, count);
-    print_string_prefix(buffer, count);
+void print_string_twice(char* buffer, int left, int right) {
+    print_string_prefix(buffer, left, right);
+    print_string_prefix(buffer, left, right);
 }
 
 int main(int argc, char* argv[]) {
@@ -41,32 +41,31 @@ int main(int argc, char* argv[]) {
         if (read_res == 0) {
             if (current_state == PRINT && len > 0 && len <= k) {
                 buffer[len++] = '\n';
-                print_string_twice(buffer, len);
+                print_string_twice(buffer, 0, len);
             }
             break;
         }
-        int left = len;
+        int left = len, prev_left = 0;
         int right = read_res + len;
         while (left < right) {
             if (buffer[left] == '\n') {
                 left++;
                 if (current_state == PRINT) {
-                    print_string_twice(buffer, left);
+                    print_string_twice(buffer, prev_left, left);
                 } else {
                     current_state = PRINT;
                 }
-                memmove(buffer, buffer + left, right - left);
-                right = right - left;
-                left = 0;
+                prev_left = left;
             } else {
                 left++;
             }
         }
-        if (left == k + 1) {
+        if (left == k + 1 && prev_left == 0) {
             len = 0;
             current_state = IGNORE;
         } else {
-            len = left;
+            memmove(buffer, buffer + prev_left, right - prev_left);
+            len = right - prev_left;
         }
     }
     free(buffer);
